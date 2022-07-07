2c4adc809ab6c1d152d3d3344ce6e960db0c0d99
vandana <vandana@DESKTOP-KJ4NMRB>
2022-07-07 15:39:14 +0530

    even

---
+++ vandu/src/vandu/even.java
@@ -0,0 +1,13 @@
+package vandu;
+
+public class even {
+
+	public static void main(String[] args) {
+	 int n=11;
+	if(n/2==0)
+System.out.println("odd");
+	else
+		System.out.println("even");
+	}
+
+}
